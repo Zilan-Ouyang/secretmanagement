2af0b070e8da0c48a97da8213905efd578cdd3eb
ZilanOuyang <zilanouyang@gmail.com>
2019-06-11 20:19:24 -0400

    adding test.js for process.env purpose

---
+++ test.js
@@ -0,0 +1,13 @@
+
+let PK = process.env.PK;
+let MNEMONIC = process.env.MNEMONIC;
+let SECRET_ONE = process.env.SECRET_ONE
+let SECRET_TWO=process.env.SECRET_TWO
+let SECRET_THREE = process.env.SECRET_THREE 
+
+console.log(PK)
+console.log(MNEMONIC)
+console.log(SECRET_ONE)
+console.log(SECRET_TWO)
+console.log(SECRET_THREE)
+
